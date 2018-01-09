ddc2ebfe07df2cd5a73ee28731f7788189ccce5e
hoangnguyen <hoangnguyendctit@gmail.com>
2018-01-09 18:04:08 +0700

    init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hoangnguyen
+ * Date: 09/01/2018
+ * Time: 16:58
+ */
+?>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+<h1>test view</h1>
+<p class="alert alert-danger"> ahihi</p>
